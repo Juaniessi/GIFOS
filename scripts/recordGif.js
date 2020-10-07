991eb4f45ef0be9c4782c4fb9e0970ce8943bcd2
Juaniessi <juaniessi@gmail.com>
2020-10-06 22:12:25 -0300

    agrego funcionalidad para capturar gif grabado

--- scripts/recordGif.js
+++ scripts/recordGif.js
@@ -3,14 +3,23 @@ let startBtn = document.getElementById("start-btn");
 let video = document.getElementById("gif-capture");
 let createTitle = document.getElementById("create-title");
 let crSubTitle = document.getElementById("create-subtitle");
-let counter = document.getElementById("counter")
+let counter = document.getElementById("counter");
+let reDoButton = counter.children[0];
+let myGifosArray = JSON.parse(localStorage.getItem("myGifos"));
+
+if (myGifosArray === null){
+	myGifosArray = [];
+}
+
+let recorder;
+let gif;
 
 startBtn.addEventListener("click", recordProcess);
-let cont = 0;
+let stageCont = 0;
 
 function recordProcess(){
 
-	switch (cont) {
+	switch (stageCont) {
 		case 0:
 			firstStage();
 			break;
@@ -50,7 +59,7 @@ function firstStage(){
 	// successCallback
 	function (localMediaStream) {
 		video.srcObject = localMediaStream;
-		cont++;
+		stageCont++;
 		recordProcess();
 	},
 
@@ -58,7 +67,7 @@ function firstStage(){
 	function(err) {
 		alert("Debes dar permiso para grabar");
 		console.log("Ocurrió el siguiente error: " + err);
-		cont = 0;
+		stageCont = 0;
 
 	});
 }
@@ -72,34 +81,83 @@ function secondStage(){
 	crSubTitle.classList.add("hidden");
 	stage1.classList.remove("isCurrent");
 	stage2.classList.add("isCurrent");
-	cont++;
-
+	stageCont++;
 }
 
 function thirdStage(){
 	// Iniciar grabación
+
+	navigator.mediaDevices.getUserMedia({
+		video: true
+	}).then(async function(stream) {
+		recorder = RecordRTC(stream, {
+			type: 'gif',
+			frameRate: 1,
+			quality: 10,
+			width: 360,
+			hidden: 240,
+			onGifRecordingStarted: function() {
+			 console.log('started')
+			}
+		});
+		recorder.startRecording();
+	});
+	
 	counter.classList.add("counter");
 	counter.classList.remove("hidden");
-	counter.children[0].textContent	 = "00:00:00"
+	reDoButton.textContent	 = "00:00:00"
 	startBtn.textContent = "FINALIZAR";
-	cont++;
+	stageCont++;
 }
 
 function fourthStage() {
 	//Agregar evento para repetir captura y
 	//Resetear todos los botones al estado correspondiente
-	startBtn.textContent = "SUBIR GIFO";
-	counter.children[0].textContent = "REPETIR CAPTURA";
-	counter.children[0].classList.add("special-hover");
+	recorder.stopRecording(onStop);
 	
-	cont++;
+	gif = recorder.getBlob();
+
+	startBtn.textContent = "SUBIR GIFO";
+	reDoButton.textContent = "REPETIR CAPTURA";
+	reDoButton.classList.add("special-hover");
+	
+	reDoButton.addEventListener('click', () => {
+		stageCont = 1;
+		reDoButton.classList.remove('special-hover');
+		counter.classList.add("hidden");
+		reDoButton.textContent = '';
+		recordProcess();
+	})
+
+	stageCont++;
 
 }
 
-function fifthStage() {
+async function fifthStage() {
+
+	let form = new FormData();
+	form.append('file', gif, 'newGif.gif');
+
+	let resp = await fetch(`https://upload.giphy.com/v1/gifs?=${form}&api_key=${apiKey}`, {
+		method: 'POST',
+		body: form,
+		json: true
+});
+	let data = await resp.json();
+	console.log(data.data);
+
+	myGifosArray.push(data.data.id);
+
+	localStorage.setItem('myGifos', JSON.stringify(myGifosArray));
+
 	counter.classList.add("hidden");
 	counter.classList.remove("counter");
 	startBtn.classList.add("hidden");
 	stage2.classList.remove("isCurrent");
 	stage3.classList.add("isCurrent");
+}
+
+function onStop() {
+	//Generar el archivo para subir
+	console.log('Supercalifragilisticuespialidoso')
 }
